d5dfe862f7bc4489ae1c63a44807782d2e62040f
Maria-cris <maria.delacruz@lottuseducation.com>
2025-12-15 18:05:56 -0600

    revert

--- app/ULA/index.php
+++ app/ULA/index.php
@@ -1,82 +1,82 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('content-type: application/json; charset=utf-8');
-// header('Access-Control-Allow-Methods: "GET, PUT, POST, DELETE, HEAD, OPTIONS"');
 
-// add_action( 'rest_api_init', function () {
-//   register_rest_route('wp', '/json', array(
-//     'methods' => 'GET',
-//     'callback' => 'get_programa_online_presencial',
-//   ) );
-// } );
+extract($_POST);
+if($tk == "ptk" && $_POST){
+	function get_sf_auth_data() {
+		
+		header('Access-Control-Allow-Origin: *');
+		header('Content-Type: application/json');
+		$post_data = array(
+			'grant_type'    => 'password',
+			'client_id'     => '3MVG9LBJLApeX_PAaOZZnyLX4MdUEQQ3Uvh67OB.CUsoBo5VPlhI7Y92gdagNcjsXblunkDI18KLBA6nY2tL7',
+			'client_secret' => '011D2286679B186BFB5928E679F069E9E9F81ACDFCE7CE3F713D65CD713B8309',
+			'username'      => 'ula_integracion@lottuseducation.com',
+			'password'      => 'CRMLottus2022*e8xPqNbPm9B7rkJaKuOLr8GR'
+		);
+		
+		$headers = array(
+			'Content-type' => 'application/x-www-form-urlencoded;charset=UTF-8'
+		);
 
-//Función Callback
-function get_programa_online_presencial( ) {
- /**
- * Global vars
- * TODO: Move to a safe route
- */
-    $urlAccessToken    = 'https://lottus.my.salesforce.com/services/oauth2/token?client_id=3MVG9LBJLApeX_PAaOZZnyLX4MdUEQQ3Uvh67OB.CUsoBo5VPlhI7Y92gdagNcjsXblunkDI18KLBA6nY2tL7&client_secret=011D2286679B186BFB5928E679F069E9E9F81ACDFCE7CE3F713D65CD713B8309&username=uteg_integracion@lottuseducation.com&password=CRMLottusUTEG2022*bNLHlVttBLweSxnGqvbWFx3f1&grant_type=password';
-    $urlCourseOffering = 'https://lottus.my.salesforce.com/services/apexrest/hed__Course_Offering__c?linea=ULA';
-    // $urlCourseOfferingUTEG = 'https://lottus.my.salesforce.com/services/apexrest/hed__Course_Offering__c?linea=UTEG';
-    // $urlCourseOfferingUANE = 'https://lottus.my.salesforce.com/services/apexrest/hed__Course_Offering__c?linea=UANE';
-    $grantType         = 'password';
-    $clientId          = '3MVG9LBJLApeX_PAaOZZnyLX4MdUEQQ3Uvh67OB.CUsoBo5VPlhI7Y92gdagNcjsXblunkDI18KLBA6nY2tL7';
-    $clientSecret      = '011D2286679B186BFB5928E679F069E9E9F81ACDFCE7CE3F713D65CD713B8309';
-    $username          = 'ula_integracion@lottuseducation.com';
-    $password          = 'CRMLottus2022*e8xPqNbPm9B7rkJaKuOLr8GR';
+		$curl = curl_init('https://lottus.my.salesforce.com/services/oauth2/token');
+		
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
 
-    // Access request
-    $content = [
-        'grant_type'    => $grantType,
-        'client_id'     => $clientId,
-        'client_secret' => $clientSecret,
-        'username'      => $username,
-        'password'      => $password
-    ];
-    $options = [
-        'http' => [
-            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-            'method'  => 'POST',
-            'content' => http_build_query($content)
-        ]
-    ];
-    $context  = stream_context_create($options);
-    $access =  file_get_contents($urlAccessToken, false, $context);
+		$response = curl_exec($curl);
+		curl_close($curl);
 
-    if($access === false) {
-        // ERROR MSG
-        // $e = error_get_last();
-        // echo "error: " . $e["message"];
-        echo "Error: No se pudo consultar la oferta académica en estos momentos (1)";
-    } else {
-        $token = json_decode($access);
-        // Offering request
-        $options = [
-            'http' => [
-                'header' => "Authorization: {$token->token_type} {$token->access_token}\r\n",
-                'method' => 'GET'
-            ]
-        ];
-        $context  = stream_context_create($options);
-        $offering =  file_get_contents($urlCourseOffering, false, $context);
-        // $offeringUTEG =  file_get_contents($urlCourseOfferingUTEG, false, $context);
-        // $offeringUANE =  file_get_contents($urlCourseOfferingUANE, false, $context);
-        if($offering === false && $offeringUTEG  === false) {
-            // ERROR MSG
-            // $e = error_get_last();
-            // echo "error: " . $e["message"];
-            echo "Error: No se pudo consultar la oferta académica en estos momentos (2)";
-        } else {
-            //$offering =  json_decode($offering);
-            // Example
-            //  echo "<pre>";
-            //  var_dump($offering);
-            //  echo "</pre>";
-                echo json_encode(array_merge(json_decode($offering, true)),\JSON_UNESCAPED_UNICODE);
-        }
-    }
+		
+		$sf_access_data = json_decode($response, true);
+		
+
+		return $sf_access_data;
+	}
+
+    $sf_access_data = get_sf_auth_data();
+	$instance_url=$sf_access_data['instance_url'];
+	$url = "$instance_url/services/apexrest/hed__Course_Offering__c?linea=ULA";
+
+	$headers = array(
+		"Authorization: OAuth " . $sf_access_data['access_token'],
+		'Content-Type: application/json',
+	);
+
+	$curl = curl_init($url);
+	
+	curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+	
+	curl_setopt($curl, CURLOPT_HEADER, false);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+	$json_response = curl_exec($curl);
+	curl_close($curl);
+    
+	$datos = array();
+	$datos2 = array();
+	$dat = json_decode($json_response,true);
+	
+	for($i=0;$i < count($dat);$i++){
+		$datos[] = $dat[$i];
+	}
+	
+	for($i=0;$i < count($dat);$i++){
+		if($datos[$i]['nombreCampus']!="UTEG ONLINE" && $datos[$i]['nombreCampus']!="MONTERREY" && $datos[$i]['nombreCampus']!="SALTILLO" && $datos[$i]['nombreCampus']!="SALTILLO" && $datos[$i]['nombreCampus']!="AMERICAS" && $datos[$i]['nombreCampus']!="CAMPUS" && $datos[$i]['nombreCampus']!="CAMPUS ZAPOPAN" && $datos[$i]['nombreCampus']!="UTEG BY ULA" && $datos[$i]['nombreCampus']!="TLAJOMULCO" && $datos[$i]['nombreCampus']!="TORREÓN" && $datos[$i]['nombreCampus']!="MATAMOROS" && $datos[$i]['nombreCampus']!="PIEDRAS NEGRAS" && $datos[$i]['nombreCampus']!="LAZARO CARDENAS" && $datos[$i]['nombreCampus']!="OLIMPICA" && $datos[$i]['nombreCampus']!="RIO NILO" && $datos[$i]['nombreCampus']!="UANE ONLINE" && $datos[$i]['nombreCampus']!="REYNOSA" && $datos[$i]['nombreCampus']!="SABINAS" && $datos[$i]['nombreCampus']!="PEDRO MORENO" && $datos[$i]['nombreCampus']!="MONCLOVA"){
+		  $datos2[] = $datos[$i];
+	    }
+	}
+	
+	echo json_encode($datos2);
+	
+}else{
+	echo "acceso denegado";
 }
-get_programa_online_presencial()
-?> 
+	?>
